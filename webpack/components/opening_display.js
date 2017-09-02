88e7714df2bd61ba6ebf926161a77253f0a00aa5
osa10928 <osa10928@gmail.com>
2017-09-02 11:14:20 -0500

    6th onclick try

--- webpack/components/opening_display.js
+++ webpack/components/opening_display.js
@@ -15,7 +15,7 @@ class OpeningDisplay extends React.Component {
 		return (
 			<div id="opening-display">
 			    <div id="options">
-			        <a href='#' className="row" id="webpages-option">
+			        <a href='###' className="row" id="webpages-option">
 			            <div className="option col text-center btn-lg" onClick={this.onClickHandler}>
 			                Webpages
 			            </div>
