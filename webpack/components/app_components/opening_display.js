a5d465be2c4592855cb26dacd071baa0eb17faac
osa10928 <osa10928@gmail.com>
2017-09-16 15:27:48 -0500

    remove transition

--- webpack/components/app_components/opening_display.js
+++ webpack/components/app_components/opening_display.js
@@ -3,16 +3,8 @@ import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 
 
 class OpeningDisplay extends React.Component {
-	constructor(props) {
-		super(props)
-		this.onClickHandler = this.onClickHandler.bind(this)
-	}
 
-	onClickHandler(e){
-		e.preventDefault();
-		e.stopPropagation();
-		this.props.onClick(e.target.id)
-	}
+	
 
 	
 
@@ -21,16 +13,20 @@ class OpeningDisplay extends React.Component {
 		return (
 	        <div id="opening-display">
 		        <div id="options">
-			        <a href='###' onClick={this.onClickHandler} className="row" id="webpages-option">
-			            <div id="webpages-button" className="option col text-center btn-lg">
-			                Webpages
-			            </div>
-			        </a>
-			        <a href='#' onClick={this.onClickHandler} className="row" id="codepens-option">
-			            <div id="codepens-button" className="option col text-center btn-lg">
-			                Codepens
-			            </div>
-			        </a>
+		            <Link to="/webpages">
+			            <a href='###' className="row" id="webpages-option">
+			                <div id="webpages-button" className="option col text-center btn-lg">
+			                   Webpages
+			                </div>
+			            </a>
+			        </Link>
+			        <Link to="/codepens">
+			            <a href='#' className="row" id="codepens-option">
+			                <div id="codepens-button" className="option col text-center btn-lg">
+			                    Codepens
+			                </div>
+			            </a>
+			        </Link>
 		        </div>
 	        </div>
         )
--- webpack/components/main_app.js
+++ webpack/components/main_app.js
@@ -1,105 +1,40 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom';
 import OpeningTitle from './app_components/opening_title';
 import OpeningDisplay from './app_components/opening_display';
 import WebpagesTitle from './app_components/webpages_title';
 import WebpagesDisplay from './app_components/webpages_display';
 import CodepensTitle from './app_components/codepens_title';
 import CodepensDisplay from './app_components/codepens_display';
+import CSSTransitionGroup from 'react-transition-group';
 
 
 class MainApp extends React.Component {
 	constructor(props){
 		super(props)
-		this.handleClick = this.handleClick.bind(this)
-		this.transitionHide = this.transitionHide.bind(this)
-		this.transitionShow = this.transitionShow.bind(this)
-		this.transitionEndEventName = this.transitionEndEventName.bind(this)
-		this.removeEvent = this.removeEvent.bind(this)
 	}
 
-	transitionEndEventName () {
-        var el = document.createElement('div')//what the hack is bootstrap
-
-        var transEndEventNames = {
-            WebkitTransition : 'webkitTransitionEnd',
-            MozTransition    : 'transitionend',
-            OTransition      : 'oTransitionEnd otransitionend',
-            transition       : 'transitionend'
-        }
-
-        for (var name in transEndEventNames) {
-            if (el.style[name] !== undefined) {
-                return transEndEventNames[name];
-            }
-        }
-        return false // explicit for ie8 (  ._.)
-    }
-
-	handleClick(destination) {
-		switch (destination) {
-			case 'webpages-button':
-			    this.transitionHide('webpages-route')
-			    break;
-
-			case 'codepens-button':
-			    this.transitionHide('codepens-route')
-			    break;
-		}
-	}
-
-	transitionHide(route){
-		console.log('called')
-		var app = document.getElementById('App')
-		var transitionEnd = this.transitionEndEventName()
-		app.addEventListener(transitionEnd, this.removeEvent(route), false)
-		//app.classList.contains('show-element') ? app.classList.remove('show-element') : null
-		//app.classList.add('remove-element')
-	}
-
-	transitionShow() {
-		//var app = document.getElementById('App')
-		//setTimeout(function(){
-		//	app.classList.contains('remove-element') ? app.classList.remove('remove-element') : null
-		//  app.classList.add('show-element')
-		//}, 600)
-	}
-
-	removeEvent(route) {
-		console.log('call-me')
-		var app = document.getElementById('App')
-		var transitionEnd = this.transitionEndEventName()
-		app.removeEventListener(transitionEnd, () => {this.removeEvent(route); }, false)
-		setTimeout(function(){
-			document.getElementById(route).click();
-		}, 500)
-	}
+	
 
 	render() {
-		console.log('lol')
-		var app = document.getElementById('App')
-		if (window.getComputedStyle(app)['max-height'] == '0px') {
-			this.transitionShow()
-		}
+		
 		return (
 			<div className="container-fluid" id="main-container">
-			    <div id="route-buttons">
-	                <Link to="/webpages">
-	                    <button id="webpages-route"></button>
-	                </Link>
-	                <Link to='/codepens'>
-	                    <button id="codepens-route"></button>
-	                </Link>
-	            </div>
+			    
 			    <section id="main-info">
-			        <Route exact path="/" component={OpeningTitle} />
-			        <Route exact path="/webpages" component={WebpagesTitle} />
-			        <Route exact path="/codepens" component={CodepensTitle} />
+			            <Switch key={location.pathname} location={location}>
+			                <Route exact path="/" component={OpeningTitle} />
+			                <Route exact path="/webpages" component={WebpagesTitle} />
+			                <Route exact path="/codepens" component={CodepensTitle} />
+			            </Switch>
+			     
 			    </section>
 			    <section id="main-display">
-			        <Route exact path="/" render={(props) => <OpeningDisplay onClick={this.handleClick} {...props} />} />
-			        <Route exact path="/webpages" component={WebpagesDisplay} />
-			        <Route exact path="/codepens" component={CodepensDisplay} />
+			        
+			            <Route exact path="/" component={OpeningDisplay} />
+			            <Route exact path="/webpages" component={WebpagesDisplay} />
+			            <Route exact path="/codepens" component={CodepensDisplay} />
+			        
 			    </section>
 			</div>
 		)
